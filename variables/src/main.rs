06e7d626d35b511302d6c6e78b531e94ecf52433
1xahmed <yourgitemail@example.com>
2024-06-03 10:11:21 +0500

    functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
- fn main() {
-    let n: i32 = 5;
+fn double(n: i32) -> i32 {
+    n * 2
+}
 
+fn main() {
+    let n = double(25);
+    println!("{}", double(4));
     println!("{n}");
     let n: () = {
         let n = 76;
