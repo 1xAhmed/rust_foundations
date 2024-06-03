8adcba22fd4f8118d550980b19f30b39e21c4007
1xahmed <yourgitemail@example.com>
2024-06-03 10:21:49 +0500

    functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,12 +2,31 @@ fn double(n: i32) -> i32 {
     n * 2
 }
 
+fn double_or_nothing(n: i32) -> i32 {
+    if n > 0 {
+        return n * 2;
+    }
+    0
+}
+
 fn main() {
-    let n = double(25);
+    let n = double(2);
     println!("{}", double(4));
+    let p = double_or_nothing(n);
+    print!("p {p}");
     println!("{n}");
+    let i = 5;
+    let m = if i ==5 {
+        6
+    } else {
+        7
+    };
+
+    println!("{m}");
+
     let n: () = {
         let n = 76;
+        println!("{n:?}");
     };
 
     println!("{n:?}");
