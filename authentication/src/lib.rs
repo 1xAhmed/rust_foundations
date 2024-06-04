d4b3858dd44f82ddac36e5befaf951ccfaace891
1xahmed <yourgitemail@example.com>
2024-06-04 15:19:30 +0500

    Enum extend functionality

---
+++ authentication/src/lib.rs
@@ -0,0 +1,57 @@
+pub fn greet_user(name: &str) -> String {
+    format!("Hello {name}")
+}
+
+pub fn read_line() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Stdin not working");
+    input.trim().to_string()
+}
+
+#[derive(PartialEq,Debug)]
+pub enum LoginAction {
+    Granted(LoginRole),
+    Denied,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum LoginRole {
+    Admin,
+    User, 
+}
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let username = username.to_lowercase();
+
+    if username != "admin" && username != "bob" {
+        return None;
+    }
+
+    if username == "admin" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::Admin))
+    } else if username == "bob" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::User))
+    } else {
+        Some(LoginAction::Denied)
+    }
+}
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greet_user() {
+        assert_eq!("Hello Robert", greet_user("Robert"));
+    }
+
+    #[test]
+    fn test_login() {
+
+        assert_eq!(login("admin",  "password"), Some(LoginAction::Granted(LoginRole::Admin)));
+        assert_eq!(login("bob",  "password"), Some(LoginAction::Granted(LoginRole::User)));
+        // assert_eq!(login("denied",  "password"), Some(LoginAction::Denied));
+
+    }
+    
+}
