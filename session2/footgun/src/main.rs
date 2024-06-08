09c535e515361e55a323d9de17b0f2794eed2f34
1xahmed <yourgitemail@example.com>
2024-06-08 18:31:56 +0500

    Sgaring Data with Atmoics -- Included safe and unsafe code

--- session2/footgun/src/main.rs
+++ session2/footgun/src/main.rs
@@ -18,3 +18,34 @@ fn main() {
     handles.into_iter().for_each(|h| h.join().unwrap());
     println!("{}", COUNTER.load(std::sync::atomic::Ordering::Relaxed));
 }
+ 
+
+
+ /* Unsafe Version, bugs: Race condition
+
+
+
+ static mut COUNTER: i32 = 0;
+
+fn main() {
+    let mut handles = Vec::new();
+    for _ in 0..1000 {
+        let handle = std::thread::spawn(|| {
+            for _ in 0..1_100 {
+                unsafe {
+                    COUNTER += 1;
+                }
+            }
+        });
+        handles.push(handle);
+    }
+
+    handles.into_iter().for_each(|h| h.join().unwrap());
+    unsafe {
+        println!("{COUNTER}");
+    }
+}
+
+ 
+ 
+  */
