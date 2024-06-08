e17717fc2e978a424ba409803de0492779f0a460
1xahmed <yourgitemail@example.com>
2024-06-08 10:33:21 +0500

    Start a CLI App -- Login Manager Part 1

---
+++ login_manager/src/main.rs
@@ -0,0 +1,51 @@
+use authentication::get_users;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command()]
+struct  Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all users.
+    List,
+    ///Add a user.
+    Add {
+        /// The user's login name
+        username: String,
+        /// The user's password (plaintext)
+        password: String,
+        ///Optional - mark as admin
+        admin: Option<bool>
+    }
+}
+
+fn list_users() {
+    println!("{:<20}{:<20?}", "Username", "Role");
+    println!("{:-<40}", "");
+    let  users = get_users();
+    users
+        .iter()
+        .for_each(|(_, user) | {
+            println!("{:<20}{:<20?}", user.username, user.role);
+        });
+}
+
+fn main() {
+    let cli = Args::parse();
+    match cli.command {
+        Some(Commands::List) => {
+            list_users()
+        }
+        Some(Commands::Add { username, password, admin }) => {
+            println!("Add a user");
+        }
+        None => {
+            println!("Run with --help to see instructions");
+        }
+    }
+}
