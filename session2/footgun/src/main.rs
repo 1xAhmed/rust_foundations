49e85f08a9e0d044bf0efcdd66c64d24f85c1b66
1xahmed <yourgitemail@example.com>
2024-06-08 18:28:42 +0500

    Safe way to do this, using Atomics

--- session2/footgun/src/main.rs
+++ session2/footgun/src/main.rs
@@ -1,20 +1,20 @@
-static mut COUNTER: i32 = 0;
+// static mut COUNTER: i32 = 0;
+
+use std::sync::atomic::AtomicI32;
+
+static COUNTER: AtomicI32 = AtomicI32::new(0);
 
 fn main() {
     let mut handles = Vec::new();
     for _ in 0..1000 {
         let handle = std::thread::spawn(|| {
             for _ in 0..1_100 {
-                unsafe {
-                    COUNTER += 1;
-                }
+                COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
             }
         });
         handles.push(handle);
     }
 
     handles.into_iter().for_each(|h| h.join().unwrap());
-    unsafe {
-        println!("{COUNTER}");
-    }
+    println!("{}", COUNTER.load(std::sync::atomic::Ordering::Relaxed));
 }
